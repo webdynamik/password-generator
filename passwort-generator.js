871d3c1699f89f23e431659fde513e225beba6a1
Jan <jan@webdynamik.de>
2014-11-13 00:12:05 +0100

    version update

--- passwort-generator.js
+++ passwort-generator.js
@@ -1,4 +1,4 @@
-// passwort-generator.js 1.1.0
+// passwort-generator.js 1.1.1
 // http://passwort-generieren.de
 // (c) 2014 Jan Krause
 (function() {
@@ -26,7 +26,7 @@
     }
 
     // Current version.
-    PasswortGenerator.VERSION = '1.1.0';
+    PasswortGenerator.VERSION = '1.1.1';
 
     //base
     PasswortGenerator._document = document;
