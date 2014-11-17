3216a45a1a7512255d76fd868b0df7d2ed98aef0
Jan <jan@webdynamik.de>
2014-11-17 03:03:39 +0100

    version 1.5.0 + add grunt-yslow

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,15 +34,38 @@ module.exports = function(grunt) {
 //              }
 //          }
       },
-
+      yslow: {
+          options: {
+              thresholds: {
+                  weight: 180,
+                  speed: 1000,
+                  score: 80,
+                  requests: 15
+              }
+          },
+          pages: {
+              files: [
+                  {
+                      src: 'http://localhost:80/passwort-generator/tests/index.html',
+                      thresholds: {
+                          weight: 280
+                      }
+                  },
+                  {
+                      src: 'http://localhost:80/passwort-generator/index.html'
+                  }
+              ]
+          }
+      },
       taskName: {}
   });
 
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-yslow');
 
-    grunt.registerTask('all', ['jshint','qunit', 'shell']);
+    grunt.registerTask('all', ['jshint','qunit', 'shell', 'yslow']);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['all']);
