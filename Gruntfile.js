9c28e9e12ebacb6609dc517b1eb1d9c3eae0fb74
Jan <jan@webdynamik.de>
2014-11-17 02:50:40 +0100

    add basic phantom js example

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'), // the package file to use
-
+      shell: {
+          phantom: {
+              command: 'phantomjs tests/phantom/loadspeed.js http://localhost:80/passwort-generator/tests/index.html',
+              options: {
+                  stdout: true
+              }
+          }
+      },
       jshint: {
           all: [
               'password-generator.js',
@@ -19,86 +26,24 @@ module.exports = function(grunt) {
       // Unit tests.
       qunit: {
           all_tests: ['tests/*.html'],
-          urls: {
-              options: {
-                  urls: [
-                      'http://localhost:80/passwort-generator/tests/index.html',
-                  ]
-              }
-          }
-      },
-
-      phantom_test: {
-          basic: {
-              options: {
-                  url: 'http://localhost:80/passwort-generator/index.html',
-                  expected: [1, 2, 3, 4, 5, 6],
-                  test: function test(a, b, c) {
-                      if (!test.actual) { test.actual = []; }
-                      test.actual.push(a, b, c);
-                  }
-              }
-          }
+//          urls: {
+//              options: {
+//                  urls: [
+//                      'http://localhost:80/passwort-generator/tests/index.html',
+//                  ]
+//              }
+//          }
       },
 
       taskName: {}
   });
 
+    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-
-    // The most basic of tests. Not even remotely comprehensive.
-    grunt.registerMultiTask('phantom_test', 'A test, of sorts.', function() {
-        var options = this.options();
-        var phantomjs = require('./lib/phantomjs').init(grunt);
-// Load up and Instantiate the test server
-        if (options.server) { require(options.server); }
-// Do something.
-        phantomjs.on('test', options.test);
-        phantomjs.on('done', phantomjs.halt);
-        phantomjs.on('debug', function(msg) {
-            grunt.log.writeln('debug:' + msg);
-        });
-// Built-in error handlers.
-        phantomjs.on('fail.load', function(url) {
-            phantomjs.halt();
-            grunt.verbose.write('Running PhantomJS...').or.write('...');
-            grunt.log.error();
-            grunt.warn('PhantomJS unable to load "' + url + '" URI.');
-        });
-        phantomjs.on('fail.timeout', function() {
-            phantomjs.halt();
-            grunt.log.writeln();
-            grunt.warn('PhantomJS timed out.');
-        });
-// This task is async.
-        var done = this.async();
-// Spawn phantomjs
-        phantomjs.spawn(options.url, {
-// Additional PhantomJS options.
-            options: options.phantomJSOptions,
-// Complete the task when done.
-            done: function(err) {
-                if (err) { done(err); return; }
-                var assert = require('assert');
-                var difflet = require('difflet')({indent: 2, comment: true});
-                try {
-                    assert.deepEqual(options.test.actual, options.expected, 'Actual should match expected.');
-                    grunt.log.writeln('Test passed.');
-                    done();
-                } catch (err) {
-                    grunt.log.subhead('Assertion Failure');
-                    console.log(difflet.compare(err.expected, err.actual));
-                    done(err);
-                }
-            }
-        });
-    });
-
-
-    grunt.registerTask('test', ['jshint','qunit']);
+    grunt.registerTask('all', ['jshint','qunit', 'shell']);
 
     // By default, lint and run all tests.
-    grunt.registerTask('default', ['test']);
+    grunt.registerTask('default', ['all']);
 };
